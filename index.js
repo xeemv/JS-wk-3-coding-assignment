bd47455b0ff2334666c8551236ac303cbcf9c5f8
xeemv <xeeyang08@gmail.com>
2021-12-15 18:44:10 -0600

    finished 1.a

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
     // a.	Programmatically subtract the value of the first element in the array from the value in the last element of the array (do not use numbers to reference the last element, find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console.
     // b.	Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
     // c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console.
- var ages = [3, 9, 23, 64, 2, 8, 28, 93];
-
+    
+    // section 1.a. 
+ var ages = [3, 9, 23, 64, 2, 8, 28, 93]; //array for ages
 
  function firstLast(array){
-     let anArray = [];
-     anArray.push(array[0], array[array.length - 1]); {
-     }
-     return anArray;
+     let anArray = (array[array.length - 1] - array[0]); 
+     //code substract last value from the first value (idex is 0) there for subtraction will need to be reverse of what I originally thought
+    //  my thought was to write it as 3 - 93 => incorrect as it gave me a -90.
+    return anArray;
  };
  console.log(firstLast([3, 9, 23, 64, 2, 8, 28, 93]))
 
